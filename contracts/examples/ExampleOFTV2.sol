a1eecbe67c8d178d8491c3c632fc356533774bc8
IlyaPrigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2023-07-10 15:16:46 +0800

    Update ExampleOFTV2.sol

--- contracts/examples/ExampleOFTV2.sol
+++ contracts/examples/ExampleOFTV2.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "../token/oft/v2/OFTV2.sol";
-contract ExampleOFTV201 is OFTV2 {
-    constructor(address _layerZeroEndpoint, uint _initialSupply, uint8 _sharedDecimals) OFTV2("ExampleOFT", "OFT", _sharedDecimals, _layerZeroEndpoint) {
+contract ExampleOFTV202 is OFTV2 {
+    constructor(address _layerZeroEndpoint, uint _initialSupply, uint8 _sharedDecimals) OFTV2("ExampleOFTV202", "OFT202", _sharedDecimals, _layerZeroEndpoint) {
         _mint(_msgSender(), _initialSupply);
     }
 }
