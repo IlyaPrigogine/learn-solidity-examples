72c54681144ec63e3fb7c17656110b04897a7cbd
IlyaPrigogine <84952580+IlyaPrigogine@users.noreply.github.com>
2023-07-11 11:14:49 +0800

    Update ExampleOFTV2.sol

--- contracts/examples/ExampleOFTV2.sol
+++ contracts/examples/ExampleOFTV2.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 import "../token/oft/v2/OFTV2.sol";
 contract ExampleOFTV204 is OFTV2 {
-    constructor(address _layerZeroEndpoint, uint _initialSupply, uint8 _sharedDecimals) OFTV2("ExampleOFTV203", "OFT203", _sharedDecimals, _layerZeroEndpoint) {
+    constructor(address _layerZeroEndpoint, uint _initialSupply, uint8 _sharedDecimals) OFTV2("ExampleOFTV204", "ExampleOFTV204", _sharedDecimals, _layerZeroEndpoint) {
         _mint(_msgSender(), _initialSupply);
     }
 }
